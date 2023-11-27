348ce3a066d28ea4dae6fb88a79788db635de19a
naimur8606 <119111473+naimur8606@users.noreply.github.com>
2023-11-27 11:57:15 +0600

    user get and post mathod added

--- index.js
+++ index.js
@@ -2,19 +2,93 @@ const express = require('express');
 const cors = require("cors");
 // const jwt = require('jsonwebtoken'); 
 require('dotenv').config()
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kc6dmnx.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const allProperties = client.db("EstateEcho").collection("Properties");
+        const allReviews = client.db("EstateEcho").collection("Reviews");
+        const allAdvertisement = client.db("EstateEcho").collection("Advertisement");
+        const allUsers = client.db("EstateEcho").collection("Users");
+
+
+        app.get("/Properties", async (req, res) => {
+            const result = await allProperties.find().toArray();
+            res.send(result);
+        })
+
+        app.get("/Properties/:id", async (req, res) => {
+            const id = req.params?.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await allProperties.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/Properties', async (req, res) => {
+            const item = req.body;
+            const result = await allProperties.insertOne(item);
+            res.send(result);
+        });
+
+        app.get("/Advertisement", async (req, res) => {
+            const result = await allAdvertisement.find().toArray();
+            res.send(result);
+        })
+
+        app.get("/Reviews", async (req, res) => {
+            const result = await allReviews.find().toArray();
+            res.send(result);
+        })
+
+        app.get("/Users", async (req, res) => {
+            const result = await allUsers.find().toArray();
+            res.send(result);
+        })
+
+        app.post("/Users", async (req, res) => {
+            const user = req?.body;
+            const result = await allUsers.insertOne(user);
+            res.send(result);
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
     res.send('EstateEcho Server  is open...')
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`EstateEcho Server is open on port ${port}`)
-  })
+})
